fcca5e01787bb0728c2f41092fd3214fc4b16bb0
yxsj245 <107445049+yxsj245@users.noreply.github.com>
2024-08-11 21:54:15 +0800

    Create data_dz.js

---
+++ data_dz.js
@@ -0,0 +1,26 @@
+// 定义类别和乐谱数据
+const data = {
+    "sky-bgm": {
+        name: "未知",
+        subcategories: [
+            // 可以无限添加小类别
+        ]
+    },
+    "songs": {
+        name: "歌曲",
+        subcategories: [
+            // 可以无限添加小类别
+        ]
+    },
+    "instrumental": {
+        name: "纯音乐",
+        subcategories: [{
+                name: "忧伤",
+                scores: [
+                    { author: "作者:木杨不归咯", title: "痴情冢", cover: "https://imgos.cn/2024/08/11/66b8b29366432.jpg", download: "https://oss.xiaozhuhome.site/Sky%E6%9B%B2%E8%B0%B1/%E7%BA%AF%E9%9F%B3%E4%B9%90/%E5%BF%A7%E4%BC%A4/%E7%97%B4%E6%83%85%E5%86%A2.json", authorLink: "https://www.bilibili.com/video/BV1bV411v7xy/?p=2&share_source=copy_web&vd_source=6fdda38be5eb9fcf9f074fd04e9bf9ae" }
+                ]
+            }
+            // 可以无限添加小类别
+        ]
+    }
+};
